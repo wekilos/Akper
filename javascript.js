f06936b33a9fd6c4aea10e460c8ec75751a0e020
wekilos <wekilyazgulyyew2000@gmail.com>
2022-01-25 20:49:19 +0500

    salama

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+
+
+function myFunction(){
+    let a = 0;
+    var b = 2;
+    const c = 3;
+
+    let h1 = document.getElementById("h1");
+
+    let san = h1.innerHTML;
+
+    
+
+    h1.innerHTML = +san+1
+
+}
